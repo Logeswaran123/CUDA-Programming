a29fb72044b816f04d17e0418d775342ba7100ce
Loges <logiflash99037@gmail.com>
2022-11-27 19:03:04 +0530

    Update warp_shuffle.cu

--- 11_warp_shuffle/warp_shuffle.cu
+++ 11_warp_shuffle/warp_shuffle.cu
@@ -37,6 +37,41 @@ __global__ void WarpShuffleXOR(int *input, int *output, int laneMask, int width=
     output[threadIdx.x] = out;
 }
 
+__global__ void ReduceSumWarpShuffle(int *input, int *output, int size, int width=WARP_SIZE) {
+	int tid = threadIdx.x;
+	int block_offset = blockIdx.x * blockDim.x * 2;
+	int index = block_offset + tid;
+
+	int *i_data = input + block_offset;
+
+	if ((index + blockDim.x) < size) {
+		input[index] += input[index + blockDim.x];
+	}
+	__syncthreads();
+
+	// Loop unrolling
+	for (int offset = blockDim.x / 2; offset >= 32; offset = offset / 2) {
+		if (tid < offset) {
+			i_data[tid] += i_data[tid + offset];
+		}
+		__syncthreads();
+	}
+
+    int sum = i_data[tid];
+	// Warp unrolling
+	if (tid < 32) {
+        sum += __shfl_down_sync(MASK, sum, 16, width);
+        sum += __shfl_down_sync(MASK, sum, 8, width);
+        sum += __shfl_down_sync(MASK, sum, 4, width);
+        sum += __shfl_down_sync(MASK, sum, 2, width);
+        sum += __shfl_down_sync(MASK, sum, 1, width);
+	}
+
+	if (tid == 0) {
+		output[blockIdx.x] = sum;
+	}
+}
+
 int main() {
     int srcLane, delta, laneMask;
     int size = 64;
@@ -97,6 +132,24 @@ int main() {
     host_output = (int*)malloc(byte_size);
     GPUErrorCheck(cudaMemcpy(host_output, device_output, byte_size, cudaMemcpyDeviceToHost));
     PrintArray(host_output, size);
+    GPUErrorCheck(cudaFree(device_output));
+    free(host_output);
+
+    cout << "\nReduce Sum with Warp Shuffle:\n" << endl;
+    int cpu_result = ReductionSumCPU(host_input, size);
+
+    int temp_array_byte_size = sizeof(int)* grid.x;
+    GPUErrorCheck(cudaMalloc((void**)&device_output, temp_array_byte_size));
+    ReduceSumWarpShuffle<<<grid, block>>>(device_input, device_output, size);
+    GPUErrorCheck(cudaDeviceSynchronize());
+    host_output = (int*)malloc(temp_array_byte_size);
+    GPUErrorCheck(cudaMemcpy(host_output, device_output, temp_array_byte_size, cudaMemcpyDeviceToHost));
+
+    int gpu_result = 0;
+    for (int i = 0; i < grid.x; i++) {
+        gpu_result += host_output[i];
+    }
+    CompareResults(gpu_result, cpu_result);
     GPUErrorCheck(cudaFree(device_output));
     free(host_output);
 
