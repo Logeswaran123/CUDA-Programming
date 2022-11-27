04b7bb2f939fa86202316b0de70b6c2de743f03e
Loges <logiflash99037@gmail.com>
2022-11-27 19:05:02 +0530

    Update warp_shuffle.cu

--- 11_warp_shuffle/warp_shuffle.cu
+++ 11_warp_shuffle/warp_shuffle.cu
@@ -38,38 +38,38 @@ __global__ void WarpShuffleXOR(int *input, int *output, int laneMask, int width=
 }
 
 __global__ void ReduceSumWarpShuffle(int *input, int *output, int size, int width=WARP_SIZE) {
-	int tid = threadIdx.x;
-	int block_offset = blockIdx.x * blockDim.x * 2;
-	int index = block_offset + tid;
+    int tid = threadIdx.x;
+    int block_offset = blockIdx.x * blockDim.x * 2;
+    int index = block_offset + tid;
 
-	int *i_data = input + block_offset;
+    int *i_data = input + block_offset;
 
-	if ((index + blockDim.x) < size) {
-		input[index] += input[index + blockDim.x];
-	}
-	__syncthreads();
+    if ((index + blockDim.x) < size) {
+        input[index] += input[index + blockDim.x];
+    }
+    __syncthreads();
 
-	// Loop unrolling
-	for (int offset = blockDim.x / 2; offset >= 32; offset = offset / 2) {
-		if (tid < offset) {
-			i_data[tid] += i_data[tid + offset];
-		}
-		__syncthreads();
-	}
+    // Loop unrolling
+    for (int offset = blockDim.x / 2; offset >= 32; offset = offset / 2) {
+        if (tid < offset) {
+            i_data[tid] += i_data[tid + offset];
+        }
+        __syncthreads();
+    }
 
     int sum = i_data[tid];
-	// Warp unrolling
-	if (tid < 32) {
+    // Warp unrolling
+    if (tid < 32) {
         sum += __shfl_down_sync(MASK, sum, 16, width);
         sum += __shfl_down_sync(MASK, sum, 8, width);
         sum += __shfl_down_sync(MASK, sum, 4, width);
         sum += __shfl_down_sync(MASK, sum, 2, width);
         sum += __shfl_down_sync(MASK, sum, 1, width);
-	}
+    }
 
-	if (tid == 0) {
-		output[blockIdx.x] = sum;
-	}
+    if (tid == 0) {
+        output[blockIdx.x] = sum;
+    }
 }
 
 int main() {
